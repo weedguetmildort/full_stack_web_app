0be36461a9127b5c5651be3dccc4143468c9410b
Weedguet Mildort <108565362+weedguetmildort@users.noreply.github.com>
2024-05-02 14:31:53 -0400

    Models Initial Setup

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/* LIBRARIES */
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
@@ -8,6 +9,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -22,7 +24,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
-/* FILE STORAGE*/
+/* FILE STORAGE */
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "public/assets")
@@ -33,7 +35,10 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-/* MONGOOSE SETUP*/
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+
+/* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
