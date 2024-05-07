026ee611d20489ed593984b2e5e2286bb0b4da1e
Weedguet Mildort <108565362+weedguetmildort@users.noreply.github.com>
2024-05-06 21:07:53 -0400

    Users Setup

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js"
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
 import { verifyToken } from "./middleware/auth.js";
@@ -42,6 +43,7 @@ app.post("/auth/register", upload.single("picture"), register);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
